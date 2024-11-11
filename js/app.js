717ba05a68aecccc1c86860c406e140a16f20f47
Wendelyn Ballesteros <ballesteroswb.844.stud@cdd.edu.ph>
2024-11-11 16:44:00 +0800

    Your commit message here

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
-// Navigation from sign in to dashboard
-document.addEventListener('DOMContentLoaded', function() {
-    const loginForm = document.querySelector('form');
-    if (loginForm) {
-        loginForm.addEventListener('submit', function(event) {
-            event.preventDefault();
-            window.location.href = 'dashboard.html'; // Redirect to dashboard
-        });
-    }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwkevD5GuyL-UH_J68stlb8-yETjjdNXg",
+  authDomain: "fir-e42bb.firebaseapp.com",
+  databaseURL: "https://fir-e42bb-default-rtdb.firebaseio.com",
+  projectId: "fir-e42bb",
+  storageBucket: "fir-e42bb.firebasestorage.app",
+  messagingSenderId: "1019133485649",
+  appId: "1:1019133485649:web:476f8eff007283efcc0e70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//inputs for email and pass
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+
+//for the login button
+const submit = document.getElementById('submit');
+submit.addEventListener('click', function(event){
+    event.preventDefault()
+    alert(5)
+})
+
 
     // Walkthrough
     const slides = document.querySelectorAll('.slides');
